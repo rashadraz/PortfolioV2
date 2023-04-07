9f2d6052fa49ff593e531cc4a8bda03d789766f6
rashad <akyrashad@gmail.com>
2023-04-07 15:03:16 +0530

    navbar and main

---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+const Layout = ({ children, className = "" }) => {
+	return (
+		<div
+			className={`${className} w-full h-full inline-block z-0 bg-light p-32`}
+		>
+			{children}
+		</div>
+	);
+};
+export default Layout;
